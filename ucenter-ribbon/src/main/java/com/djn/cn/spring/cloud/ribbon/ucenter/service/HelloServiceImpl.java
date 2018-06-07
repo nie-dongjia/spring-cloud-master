74cd760913da534ca7627a85cb910b96dd1b8e72
NieDongjia <NieDongjia@10.4.0.225>
2018-06-07 17:39:42 +0800

    ribbon demo

---
+++ ucenter-ribbon/src/main/java/com/djn/cn/spring/cloud/ribbon/ucenter/service/HelloServiceImpl.java
@@ -0,0 +1,15 @@
+package com.djn.cn.spring.cloud.ribbon.ucenter.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+@Service
+public class HelloServiceImpl {
+	@Autowired
+	private RestTemplate restTemplate;
+
+	public String hiService(String name) {
+		return restTemplate.getForObject("http://SERVICE-HI/hi?name=" + name, String.class);
+	}
+}
